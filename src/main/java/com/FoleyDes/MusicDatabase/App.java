8821652d882ef03846c4110c650f0eaff2e73d89
Jigsawfoley <desfoley@hotmail.com>
2018-03-02 12:09:39 +0000

    added loj4j2

--- src/main/java/com/FoleyDes/MusicDatabase/App.java
+++ src/main/java/com/FoleyDes/MusicDatabase/App.java
@@ -3,6 +3,9 @@ package com.FoleyDes.MusicDatabase;
 import	java.util.Date;
 import	java.util.Scanner;
 
+import	org.apache.logging.log4j.LogManager;
+import	org.apache.logging.log4j.Logger;
+
 /*****************************************************************
 	*
 	*	 Date:	2018
@@ -31,10 +34,17 @@ public	class	App
 	 //define	attributes
 	 private	Scanner	someInput;
 	 private	Date	today;
+	 //	This	is	added	to	every	class	that	needs	to	log	with	one	change
+	 //	The	getLogger(	)	part	should	contain	the	name	of	the	class	its	in
+	 private	static	Logger	LOG;
 	 //	CONSTRUCTORS
 	 //............................................................
 	 public	App()
 	 {	 	
+	 	 //associate	logging	with	this	class	so	know	the	messages	that	came	from	objects	of	this	class
+	 	 LOG	=	LogManager.getLogger(App.class);
+	 	 //test	the	logging
+	 	 testLogOutput();
 	 	 this.someInput	=	new	Scanner(System.in);
 	 	 //do	something	here
 	 	 System.out.println("	\n	Soon	...	stuff	will	happen	here");	 	
@@ -73,5 +83,17 @@ public	class	App
 	 												System.out.println(args[i]);
 	 								}
 	 	 }
-	 	}
+	 	}//EOM
+	 /**
+	 	*	Test	the	Log4J2	logging
+	 	*/
+	 	private	static	void	testLogOutput()
+	 	{
+	 	 LOG.debug("Log	test:	Test	printed	on	debug");
+	 				LOG.info("Log	test:	Test	printed	on	info");
+	 				LOG.warn("Log	test:	Test	printed	on	warn");
+	 				LOG.error("Log	test:	Test	printed	on	error");
+	 				LOG.fatal("Log	test:	Test	printed	on	fatal");
+	 				LOG.info("Appending	string:	{}.",	"Application	log	test	message	-	Hi");
+	 	}//EOM
 }//EOC
